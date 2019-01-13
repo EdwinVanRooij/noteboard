f905ce8bd117f1775ba6c4aa8f6a17a8103cbbb7
Edwin van Rooij <edwinvrooij@gmail.com>
2019-01-13 13:46:05 +0100

    cleanup of unused resources

--- app/src/main/java/io/github/edwinvanrooij/noteboard/MainActivity.kt
+++ app/src/main/java/io/github/edwinvanrooij/noteboard/MainActivity.kt
@@ -42,6 +42,7 @@ class MainActivity : Activity(), IGameListener {
 
         soundManager = SoundManager(this)
 
+
         btnRepeat.setOnClickListener {
             soundManager.repeatLastSound()
         }
